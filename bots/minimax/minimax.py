6facfe4e485f9912469dcd1a6754851df1285b22
Evermeij <ewoudvermeij@gmail.com>
2018-01-18 22:16:23 +0100

    edit

--- bots/minimax/minimax.py
+++ bots/minimax/minimax.py
@@ -57,7 +57,7 @@ class Bot:
 
             # IMPLEMENT: Add a recursive function call so that 'value' will contain the
             # minimax value of 'next_state'
-            value, move = value = self.value(next_state, depth + 1)
+            value = self.value(next_state, depth + 1)[0]
 
             if maximizing(state):
                 if value > best_value:
