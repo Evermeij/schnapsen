054631298284a13762b10b55f73bbad1729c8245
Evermeij <ewoudvermeij@gmail.com>
2018-01-27 16:17:15 +0100

    edit of bots

--- train-ml-bot.py
+++ train-ml-bot.py
@@ -18,10 +18,10 @@ from bots.rdeep import rdeep
 from bots.ml import ml
 from bots.kbbot2 import kbbot2
 
-from bots.ml2.ml2 import features
+from bots.ml.ml import features
 
 # How many games to play
-GAMES = 100000
+GAMES = 20000
 
 # Which phase the game starts in
 PHASE = 1
@@ -91,6 +91,6 @@ for str in target:
 print('instances per class: {}'.format(count))
 
 # Store the model in the ml directory
-joblib.dump(model, './bots/ml2/rand-model.pkl')
+joblib.dump(model, './bots/ml/rand-model.pkl')
 
 print('Done')
